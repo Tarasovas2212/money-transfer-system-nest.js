c330b7d5d1f4f12ba8a5558265a5bc2aeb5af95d
Ihor Tarasov <ihortarasov@gmail.com>
2020-07-22 00:44:07 +0300

    file structure change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user/entities/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule],
