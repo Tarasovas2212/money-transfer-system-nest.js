45bd82730d910956badc9da8bb2b6f77af4beff4
KN18 <50597469+KN18@users.noreply.github.com>
2020-07-22 21:12:14 +0300

    Added transacton entity

---
+++ src/transaction/entities/transaction.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Card } from 'src/card/card.entity';
+
+@Entity({ name: 'transactions' })
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 0 })
+  amount: number;
+
+  @Column({ default: Date.now })
+  date: Date;
+
+  @Column({ name: 'is_aborted', default: false })
+  isAborted: boolean;
+
+  @ManyToOne(() => Card)
+  @JoinColumn({ name: 'sender_card_id' })
+  sender: Card;
+
+  @ManyToOne(() => Card)
+  @JoinColumn({ name: 'receiver_card_id' })
+  receiver: Card;
+}
