f89bb35a5f578020614629caae7f1eba27a96ecd
KN18 <50597469+KN18@users.noreply.github.com>
2020-07-22 21:22:09 +0300

    Added transaction service

--- src/transaction/services/transaction.service.ts
+++ src/transaction/services/transaction.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Transaction } from '../entities/transaction.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService extends TypeOrmCrudService<Transaction> {
+  public constructor(
+    @InjectRepository(Transaction)
+    transactionRepository: Repository<Transaction>,
+  ) {
+    super(transactionRepository);
+  }
+}
